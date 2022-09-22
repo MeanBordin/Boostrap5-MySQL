0f5d20a6b1f1827310c5e6ec117281a840eb85d3
Mean_bordin <bodinsak2544@gmail.com>
2022-09-23 01:52:51 +0700

    fixbug1 commit

--- admin/del_product.php
+++ admin/del_product.php
@@ -6,7 +6,7 @@
 
     if(mysqli_query($conn, $sql)){
         echo "<script>alert('ลบข้อมูลสำเร็จ');</script>";
-        echo "<script>window.location='invoice.php';</script>"; 
+        echo "<script>window.location='product.php';</script>"; 
     }else{
         echo "Error : {$sql} <br>" . mysqli_error($conn);
         echo "<script>alert('ไม่สามารถลบข้อมูลได้');</script>";
--- admin/product.php
+++ admin/product.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <!-- Bs icon svg -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </head>
@@ -55,10 +54,10 @@
                     </button>
                 </td>
                 <td>
-                    <button type="button" class="btn btn-danger" onclick="clikComfirm(this.href);return false;">
+                    <button type="button" class="btn btn-danger" >
                     <a  
                         class="text-white text-decoration-none" 
-                        href="del_product.php?id=<?=$row['product_id']?>">
+                        href="del_product.php?id=<?=$row['product_id']?>" onclick="clikComfirm(this.href);return false;">
                             <i class="bi bi-trash-fill"></i>
                     </a>
                     </button>
@@ -72,7 +71,7 @@
     </div>
     <script>
         function clikComfirm(mypage){
-            let ok = confirm(`คุณต้องการลบข้อมูลหรือไม่`);
+            let ok = confirm('คุณต้องการลบข้อมูลหรือไม่');
             if(ok){
                 window.location=mypage
             }
